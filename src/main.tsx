df99e4d1fbd849d9b27b33963077d5f4ca0e0c66
Kevin Nielsen <kan@fairytalesgroup.com>
2024-07-31 21:34:11 +0200

    remove button because it is elsewhere

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "@/components/ui/sonner.tsx";
-import { SignOutButton } from "@/components/SignOutButton.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <SignOutButton />
     <Toaster />
   </React.StrictMode>,
 );
